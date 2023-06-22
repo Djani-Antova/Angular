2604e8ae15d3ba0740cefa926692271e04466df1
djaniAntova <djaniantova@gmail.com>
2023-06-22 15:59:20 +0300

    Update

---
+++ 02.Course-Introduction/Starter-Lesson/index.js
@@ -0,0 +1,104 @@
+"use strict";
+// Basic Types
+let id = 5;
+let company = 'Traversy Media';
+let isPublished = true;
+let x = 'Hello';
+let age;
+let ids = [1, 2, 3, 4, 5, 6,];
+let arr = [1, 'cat', true];
+arr.push(3);
+console.log(arr);
+// Tuple
+let person = [1, 'Brad', true];
+// Tuple Array
+let employee;
+employee = [
+    [1, 'Brad'],
+    [2, 'Dave'],
+    [3, 'Rick'],
+];
+console.log(employee);
+// Union
+let pid;
+pid = '22';
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1);
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+console.log(Direction2);
+const user = {
+    id: 1,
+    name: 'John',
+};
+// Type Assertion
+let cid = 1;
+//let customerId = <number>cid;
+let customerId = cid;
+customerId = true;
+// Functions
+function addNumber(z, w) {
+    return z + w;
+}
+console.log(addNumber(15, 16));
+// Void
+function log(message) {
+    console.log(message);
+}
+log('Hello dear John!');
+log(5);
+const user1 = {
+    id: 1,
+    name: 'John',
+};
+console.log(user1.id);
+user1.id = 2;
+console.log(user1.id);
+const p1 = 1;
+const add = (x, y) => x + y;
+const sub = (x, y) => x - y;
+// Classes
+class Person {
+    //private id: number  if we want it private 
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    register() {
+        return `${this.name} is now registered`;
+    }
+}
+const brad = new Person(1, 'Brad Traversy');
+const mike = new Person(2, "Mike Jordan");
+console.log(brad, mike);
+brad.id = 5;
+console.log(brad, mike);
+console.log(brad.register());
+// Subclasses
+class Employee extends Person {
+    constructor(id, name, position) {
+        super(id, name);
+        this.position = position;
+    }
+}
+const emp = new Employee(3, 'George', 'manager');
+console.log(emp.register());
+// Generics
+function getArray(items) {
+    return new Array().concat(items);
+}
+let numArray = getArray([1, 2, 3, 4]);
+let stringArray = getArray(['brad', 'mike', 'george']);
+numArray.push(12);
