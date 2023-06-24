62c45c2dc6647eaf7be33fee9e677ae0d20f8cd3
djaniAntova <djaniantova@gmail.com>
2023-06-24 20:08:23 +0300

    Update navigation.component.ts

--- 04.Components/components-demo-app/src/app/navigation/navigation.component.ts
+++ 04.Components/components-demo-app/src/app/navigation/navigation.component.ts
@@ -5,14 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent {  
+export class NavigationComponent {
   isActive = false;
+  inputValue = 'Hello'
 
-  activeUsers = [{name: 'Mitko', age: 29}, {name: 'Pesho', age: 27}, {name: 'Mariya', age: 41}, {name: 'Katya', age: 32},]
+  activeUsers = [{ name: 'Mitko', age: 29 }, { name: 'Pesho', age: 27 }, { name: 'Mariya', age: 41 }, { name: 'Katya', age: 32 },]
 
-  handleClick(event: Event): void {
+  handleClick(event: Event, ...args: number[]): void {
     console.log(event);
-    
-    this.isActive = !this.isActive;       
+    this.isActive = !this.isActive;
+    console.log({ args });
+
   }
 }
