0523f169ea9a970065823c4f0df6fee08639deed
djaniAntova <djaniantova@gmail.com>
2023-06-24 23:14:06 +0300

    Update app.component.ts

--- 04.Components/components-demo-app/src/app/app.component.ts
+++ 04.Components/components-demo-app/src/app/app.component.ts
@@ -4,8 +4,20 @@ import { Component } from '@angular/core';
   selector: 'app-root',
 
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = '- Title from AppComponent ';
+  title = 'Title from AppComponent ';
+
+  users = [
+    { name: 'Mitko', age: 29 },
+    { name: 'Pesho', age: 27 },
+    { name: 'Mariya', age: 41 },
+    { name: 'Katya', age: 32 },
+  ];
+
+  outputChildHandler() {
+    console.log("outputChildHandler");
+       
+  }
 }
