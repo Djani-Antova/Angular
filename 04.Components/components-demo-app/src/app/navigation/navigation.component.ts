e1f0e7cc234adcf1afea0b79bd84408a2a39de14
djaniAntova <djaniantova@gmail.com>
2023-06-24 23:15:08 +0300

    Update navigation.component.ts

--- 04.Components/components-demo-app/src/app/navigation/navigation.component.ts
+++ 04.Components/components-demo-app/src/app/navigation/navigation.component.ts
@@ -1,20 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, Output, SimpleChanges, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent {
-  isActive = false;
-  inputValue = 'Hello'
+export class NavigationComponent implements OnInit, OnChanges {
+  @Input('title') titleFromApp: string = '';
+  @Input('activeUsers') activeUsers: {name: string, age: number}[] = [];
+  @Output() onChildOutput = new EventEmitter<boolean>();
 
-  activeUsers = [{ name: 'Mitko', age: 29 }, { name: 'Pesho', age: 27 }, { name: 'Mariya', age: 41 }, { name: 'Katya', age: 32 },]
+  isActive = false;
+  inputValue = 'Hello';
+
+ 
+  ngOnInit() {
+    console.log('Navigation -> before initiated');
+    
+  }
+
+  ngAfterViewInit(): void {
+    console.log('Navigation -> After initiated');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('Changes');
+    console.log(changes);
+
+  }
 
   handleClick(event: Event, ...args: number[]): void {
-    console.log(event);
     this.isActive = !this.isActive;
-    console.log({ args });
-
+    this.onChildOutput.emit(this.isActive)
   }
 }
