26f961bd000811aa00e332e2fb9612a3f05fdf9e
djaniAntova <djaniantova@gmail.com>
2023-06-28 23:47:14 +0300

    Update

--- 05.DI_Intro_to_RxJS_Services/demo-app/src/app/app.component.ts
+++ 05.DI_Intro_to_RxJS_Services/demo-app/src/app/app.component.ts
@@ -39,3 +39,33 @@ export class AppComponent {
     inputAge.value = '';
   }
 }
+
+class Wallet {
+  balance = 0;
+  name = '';
+
+  constructor(name: string, balance: number) {
+    this.balance = balance;
+    this.name = name
+  }
+}
+
+class Car {
+  model = '';
+  constructor(model: string) {
+    this.model = model;
+  }
+}
+class Person {
+  wallet: Wallet;
+  car: Car;
+
+  constructor(car: Car, wallet: Wallet) {
+    this.car = car;
+    this.wallet = wallet
+  }
+}
+
+const ivansCar = new Car('BMW');
+const ivansWallet = new Wallet('Ivan', 5000);
+const ivanPerson = new Person(ivansCar, ivansWallet)
--- 05.DI_Intro_to_RxJS_Services/demo-app/src/app/users-list/users-list.component.ts
+++ 05.DI_Intro_to_RxJS_Services/demo-app/src/app/users-list/users-list.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from '@angular/core';
 import { User } from '../types/User';
 
 @Component({
@@ -10,8 +10,16 @@ import { User } from '../types/User';
 export class UsersListComponent {
 @Input() users: User [] = [];
 
+constructor(private cd: ChangeDetectorRef) {
+
+}
+
 ngOnChanges() {
   console.log('invoked from ngOnVhanges');
   
+}
+
+refresh() {
+  this.cd.detectChanges();
 }
 }
