51c4f29e4c53c534b0488b5c1914f23c66db0d51
djaniAntova <djaniantova@gmail.com>
2023-07-02 14:32:00 +0300

    Add posts type

--- 06.Workshop Components/WORKSHOP-COMPONENTS/components-workshop/src/app/types/theme.ts
+++ 06.Workshop Components/WORKSHOP-COMPONENTS/components-workshop/src/app/types/theme.ts
@@ -1,16 +1,5 @@
-export interface UserId{
-    "themes": string[],
-    "posts": string[],
-    "_id": string,
-    "tel": string,
-    "email": string,
-    "username": string,
-    "password": string,
-    "created_at": string,
-    "updatedAt": string,
-    "__v": number
+import { UserId } from "./user-id";
 
-}
 
 
 export interface Theme {
