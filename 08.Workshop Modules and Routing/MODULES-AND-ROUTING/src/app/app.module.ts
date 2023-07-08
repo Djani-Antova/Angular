34b65d518e18bdb08ae1ea9d2d5d1aeb04b3cec0
djaniAntova <djaniantova@gmail.com>
2023-07-08 13:02:15 +0300

    Show userList

--- 08.Workshop Modules and Routing/MODULES-AND-ROUTING/src/app/app.module.ts
+++ 08.Workshop Modules and Routing/MODULES-AND-ROUTING/src/app/app.module.ts
@@ -1,14 +1,24 @@
+import { AppComponent } from './app.component';
+// import { AppRoutingModule} from './app.component' //TODO add AppRoutingModule
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module'
 import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, CoreModule, UserModule],
+  imports: [
+    BrowserModule, 
+    BrowserAnimationsModule, 
+    CoreModule, 
+    UserModule, 
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- 08.Workshop Modules and Routing/MODULES-AND-ROUTING/src/app/user/user.service.ts
+++ 08.Workshop Modules and Routing/MODULES-AND-ROUTING/src/app/user/user.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Injectable, OnInit } from '@angular/core';
+import { User } from '../types/User';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
+  export class UserService implements OnInit {
 
-  constructor() { }
-}
+    constructor(private http: HttpClient) { }
+
+    ngOnInit(): void {
+      //
+    }
+
+    fetchUsers(): Observable<User[]> {
+      return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users')
+    }
+  }
