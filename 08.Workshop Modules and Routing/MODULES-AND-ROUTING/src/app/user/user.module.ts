2aded56e1fa334ec223bc1c81461b8a82a7ef2ef
djaniAntova <djaniantova@gmail.com>
2023-07-08 11:04:04 +0300

    Add user.service

--- 08.Workshop Modules and Routing/MODULES-AND-ROUTING/src/app/user/user.module.ts
+++ 08.Workshop Modules and Routing/MODULES-AND-ROUTING/src/app/user/user.module.ts
@@ -3,14 +3,9 @@ import { CommonModule } from '@angular/common';
 import { UserListComponent } from './user-list/user-list.component';
 
 
-
 @NgModule({
-  declarations: [
-    UserListComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [UserListComponent]
+  declarations: [UserListComponent],
+  imports: [CommonModule],
+  exports: [UserListComponent],
 })
-export class UserModule { }
+export class UserModule {}
