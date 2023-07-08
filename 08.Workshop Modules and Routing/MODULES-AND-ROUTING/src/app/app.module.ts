f5092ff5bae7c9c9739c618de5e94d40dbd70b37
djaniAntova <djaniantova@gmail.com>
2023-07-08 10:45:36 +0300

    Update

--- 08.Workshop Modules and Routing/MODULES-AND-ROUTING/src/app/app.module.ts
+++ 08.Workshop Modules and Routing/MODULES-AND-ROUTING/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module'
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, CoreModule],
+  imports: [BrowserModule, BrowserAnimationsModule, CoreModule, UserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
