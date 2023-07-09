561545b2e95d1cdb08e9c81cd457de8db3022a62
djaniAntova <djaniantova@gmail.com>
2023-07-09 19:53:54 +0300

    Update

--- 08.Workshop Modules and Routing/modules-and-routing-workshop/src/app/user/user.service.ts
+++ 08.Workshop Modules and Routing/modules-and-routing-workshop/src/app/user/user.service.ts
@@ -1,14 +1,42 @@
 import { Injectable } from '@angular/core';
 import { User } from '../types/user';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  user: User | undefined
+  user: User | undefined;
+  USER_KEY = '[user]';
 
-    //here we check if there is s.th. in the localSorage
-  constructor() { 
-
+  get isLogged(): boolean {
+    return !!this.user; // to reassure it's boolean and not sometype of falsu cases that we are not interesed in
   }
+
+  constructor() { 
+    try {
+      const lsUser = localStorage.getItem(this.USER_KEY);
+      if (lsUser) {
+        this.user = JSON.parse(lsUser); // Parse the JSON string into a User object
+      }
+
+    } catch (error) {
+      this.user = undefined;
+    }
+  }
+
+  login() : void {
+    this.user = {
+      email: 'john.doe@gmail.com',
+      firstName: 'John'
+    };
+    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user))
+
+  };
+
+  logout(): void {
+    this.user = undefined;
+    localStorage.removeItem(this.USER_KEY)
+  };
 }
+
