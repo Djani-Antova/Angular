bf1a0dca8cf946cf193f6f49a512d60660cc37b7
djaniAntova <djaniantova@gmail.com>
2023-07-13 22:50:39 +0300

    Update app.module.ts

--- 09.Forms/directives-and-forms/src/app/app.module.ts
+++ 09.Forms/directives-and-forms/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighlightOnMoveDirective } from './highlight-on-move.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighlightOnMoveDirective
   ],
   imports: [
     BrowserModule,
