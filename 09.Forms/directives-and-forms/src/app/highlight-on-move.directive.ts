4d9969217084ad8e1b9fedcaf6e119adf248d75e
djaniAntova <djaniantova@gmail.com>
2023-07-13 23:08:03 +0300

    Update highlight-on-move.directive.ts

--- 09.Forms/directives-and-forms/src/app/highlight-on-move.directive.ts
+++ 09.Forms/directives-and-forms/src/app/highlight-on-move.directive.ts
@@ -1,16 +1,23 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightOnMove]'
 })
 export class HighlightOnMoveDirective implements OnInit {
 
-  constructor(private elRef: ElementRef) {    
+  constructor(private elRef: ElementRef, private renderer:Renderer2) {    
     
   }
   ngOnInit(): void {
     console.log(this.elRef);
-    this.elRef.nativeElement.style.backgroundColor = 'yellow'
+    // this.elRef.nativeElement.style.backgroundColor = 'yellow'; - this is not recommended
+
+      // here is how to do it - by Renderer2
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      'background-color', 
+      'red'
+    )
   }
 
 }
