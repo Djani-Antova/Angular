12c546e04811661fedf80160cb8193a67d2904e6
djaniAntova <djaniantova@gmail.com>
2023-07-14 21:40:08 +0300

    Generate home comp

--- 09.Forms/directives-and-forms/src/app/app.module.ts
+++ 09.Forms/directives-and-forms/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HighlightOnMoveDirective } from './highlight-on-move.directive';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HighlightOnMoveDirective
+    HighlightOnMoveDirective,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
