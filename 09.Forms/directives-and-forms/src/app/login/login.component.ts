96c37f41129d9ca5eea48933d651176753a6d850
djaniAntova <djaniantova@gmail.com>
2023-07-16 00:22:00 +0300

    Update login.component.ts

--- 09.Forms/directives-and-forms/src/app/login/login.component.ts
+++ 09.Forms/directives-and-forms/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -6,16 +6,34 @@ import { NgForm } from '@angular/forms';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
-  //another approach to get the form login without passing it as a prop
-  submitHandler(form: NgForm): void {
+export class LoginComponent implements OnInit {
+  email: string = '';
+
+  // another way to get the form login without passing it as a parameter of the callback
+  @ViewChild('loginForm') loginForm: NgForm | undefined;
+
+  // will render only the static content
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    if (this.loginForm) {
+      this.loginForm.form.valueChanges.subscribe(console.log);
+    }
+  }
+
+  // Will render the final content with the dynamic components
+  // ngAfterViewInit(): void {
+  // }
+
+  submitHandler(): void {
+    if (!this.loginForm) return;
+
+    const form = this.loginForm;
     if (form.invalid) {
       return;
     }
 
     form.setValue({ email: '', password: '' });
     const value: { email: string; password: string } = form.value;
-    console.log({ value });
-    
   }
 }
